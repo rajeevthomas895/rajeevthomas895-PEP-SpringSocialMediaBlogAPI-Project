6d6a6bcf458b5de1c21c353c156ba1dcd1991ffd
rajeevthomas895 <rajeevthomas895@github.com>
2026-04-24 18:43:29 +0000

    Test run: 12/28 passed, 16 failed, 0 skipped - 2026-04-24T18:43:29.011Z

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -14,6 +14,7 @@ import com.example.repository.MessageRepository;
 public class MessageService {
   @Autowired
   private MessageRepository messageRepository;
+  @Autowired
   private AccountRepository accountRepository;
 
   public Message createMessage(Message message){
