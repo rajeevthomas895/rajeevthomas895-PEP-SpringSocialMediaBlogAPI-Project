00e544830f313d1778f57fda4a01f52d25d88f07
rajeevthomas895 <rajeevthomas895@gmail.com>
2026-04-24 11:17:50 -0400

    Initial commit

---
+++ src/main/java/com/example/SocialMediaApp.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is a class that is used to run your application.
+ *
+ * DO NOT CHANGE ANYTHING IN THIS CLASS
+ *
+ */
+@SpringBootApplication
+public class SocialMediaApp {
+    /**
+     * Runs the application
+     * 
+     * @param args The arguments of the program.
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(SocialMediaApp.class, args);
+    }
+}
